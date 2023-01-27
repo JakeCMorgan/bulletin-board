0c8bc99fa5cc682bd7cc05b15b50baff95984cff
Jake Morgan <jakecmorgan.2000@gmail.com>
2023-01-26 16:19:19 -0800

    supabase items are grabbed and rendered on page when demo table is used

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* Imports */
-
+import { getPosts, getUser } from './fetch-utils.js';
+import { renderPostIt } from './render-utils.js';
 /* Get DOM Elements */
 const authButton = document.getElementById('auth-button');
 const createButton = document.getElementById('create');
@@ -7,6 +8,29 @@ const bulletin = document.getElementById('bulletin-board');
 /* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    const user = await getUser();
+
+    if (user) {
+        authButton.addEventListener('click', logout);
+        authButton.textContent = 'Logout';
+    } else {
+        authButton.addEventListener('click', () => {
+            location.replace('/auth');
+        });
+        authButton.textContent = 'Login';
+    }
+
+    createButton.addEventListener('click', () => {
+        location.replace('/create');
+    });
+
+    const posts = await getPosts();
+    for (let post of posts) {
+        const postDiv = renderPostIt(post);
+        bulletin.append(postDiv);
+    }
+});
 
 /* Display Functions */
 
---
+++ fetch-utils.js
@@ -0,0 +1,46 @@
+const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+
+    return response.user;
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
+export async function getPosts() {
+    const resp = await client.from('posts').select('*');
+    return checkError(resp);
+}
+
+export async function createPost(post) {
+    const resp = await client.from('posts').insert(post);
+    return checkError(resp);
+}
+
+export async function checkAuth() {
+    const user = await getUser();
+
+    if (!user) location.replace('/auth-page');
+}
+
+export async function redirectIfLoggedIn() {
+    if (await getUser()) {
+        location.replace('/');
+    }
+}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
